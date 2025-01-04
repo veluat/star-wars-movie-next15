895edf26119b6c76316b156365d1e1ef2124a4f4
Julia <veluat@gmail.com>
2025-01-04 12:45:48 +0300

    feat: add customs footer

---
+++ src/components/CustomFooter.tsx
@@ -0,0 +1,7 @@
+export const CustomFooter = () => {
+  return (
+    <footer className={'wrapper'}>
+      <small>Created by &copy;Julia Popova {new Date().getFullYear()}</small>
+    </footer>
+  );
+};
