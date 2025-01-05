cbd04f86728fe18d61ff2b5abbc40323b5ccd9de
Julia <veluat@gmail.com>
2025-01-05 12:46:07 +0300

    feat: add customs header

---
+++ src/components/CustomHeader.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export const CustomHeader = () => {
+  return (
+    <header className={'wrapper'}>
+      <Link href={"/"}>Home</Link>
+      <Link href={"/films"}>Films</Link>
+      <Link href={"/about"}>About</Link>
+    </header>
+  )
+}
