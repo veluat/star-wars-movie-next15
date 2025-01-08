381e49f7a3d8cd66ca7b9ff3564e3ac294e16084
Julia <veluat@gmail.com>
2025-01-08 12:47:09 +0300

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,11 @@
-import Image from 'next/image'
-import icon from './../../public/icon.png'
-import styles from './page.module.css';
+import Image from "next/image";
+import image from './../../public/star-wars.png'
 
-interface Film {
-  episode_id: number;
-  title: string;
-}
-
-export const revalidate = 3600;
-
-export default async function FilmList() {
-  const response = await fetch('https://swapi.dev/api/films/');
-
-  if (!response.ok) {
-    return <div>Error loading films...</div>;
-  }
-
-  const data = await response.json();
-  const films: Film[] = data.results;
-
-  const correctFilms = films.map((film, index) => ({
-    ...film,
-    episode_id: index + 1,
-  }));
-
+export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>Star Wars Films</h1>
-      <ul className={styles.list}>
-        {correctFilms.map((film) => (
-          <li key={film.episode_id} className={styles.listItem}>
-            <a href={`/film/${film.episode_id}`} className={styles.link}>
-              <div className={styles.card}>
-                <Image src={icon} alt={'Star Wars Icon'} width={25} height={25}/>
-                {film.title}
-              </div>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
-}
+    <>
+      <h1>Welcome to Star Wars films</h1>
+      <Image src={image} alt={'Star wars movie'} height={600} placeholder={'blur'}/>
+    </>
+  )
+};
