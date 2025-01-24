daf2e3dccefe9d4d48049c3fc327bc76a8917eee
Julia <veluat@gmail.com>
2025-01-24 21:25:45 +0300

    refactor: layout, css

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,10 +1,20 @@
+import styles from '@/app/(pages)/films/filmList.module.css';
+
 export default function About() {
   return (
-    <>
-      <h1>About app</h1>
-      <span>This test application is developed using App Router (Next.js 15) and demonstrates the capabilities of static
+    <div style={{ width: '100%', maxWidth: '700px', padding: '0 10px', fontSize: '18px', lineHeight: '1.7' }}>
+      <h1 className={styles.title}>About app</h1>
+      <p>This test application is developed using App Router (Next.js 15) and demonstrates the capabilities of static
         generation and incremental rebuilding. The application dynamically displays movie data from an open API.
-      </span>
-    </>
-  )
+      </p>
+      <br/>
+      <ul>
+        <li>The app allows users to navigate from the root page to any of the template movie description pages.
+        </li>
+        <li>Movie data is retrieved from the public **swapi.info** server, which is an effective and simple way to
+          demonstrate the use of static generation and incremental rebuilding in Next.js.
+        </li>
+      </ul>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import Image from "next/image";
-import image from './../../public/star-wars.png'
+import styles from '@/app/(pages)/films/filmList.module.css';
+import style from './home.module.css'
 
 export default function Home() {
   return (
     <>
-      <h1>Welcome to Star Wars films</h1>
-      <Image src={image} alt={'Star wars movie'} height={600} placeholder={'blur'}/>
+      <h1 className={styles.title}>Welcome to Star Wars films</h1>
+      <div className={style.wrapper}>
+        {/*<Image src={image}
+               alt={'Star wars movie'}
+               placeholder={'blur'}
+               fill
+               sizes="(max-width: 900px) 100vw, (max-width: 600px) 50vw, 33vw"
+               priority
+        />*/}
+      </div>
     </>
-  )
+  );
 };
